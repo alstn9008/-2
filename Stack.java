af8b9dcdcf79de650e0c7f3b223f5871ad397e62
Minsu Kim <alstn9008@naver.com>
2020-09-28 12:49:21 +0900

    Initial sharing of project

---
+++ Stack.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface Stack here.
+ *
+ * @author (2017210105 김민수)
+ * @version (20200928)
+ */
+public interface Stack<T>
+{
+    public abstract T pop();
+    public abstract boolean push(T obj);
+}
